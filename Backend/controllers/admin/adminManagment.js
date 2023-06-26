424a0b1782758dc822e26bf7054857ed5efb9016
laMola <GartalTraxtor345@yahoo.com>
2023-06-26 17:58:42 +0330

    error handling & getAllCities function added :   Backend/controllers/admin/adminManagment.js 	added routes for city handlers:   Backend/routes/adminManagment.js

--- Backend/controllers/admin/adminManagment.js
+++ Backend/controllers/admin/adminManagment.js
@@ -1,17 +1,10 @@
 const countryDB = require('../../models/country');
+const estateDB = require('../../models/estate');
 const catchAsync = require('./../../utilities/catchAsync');
-
-// exports.postAddCountry = (req, res) => {
-//     const country_name = req.body.countryName;
-//     console.log(country_name);
-//     return res.json({ message: 'Success' });
-// };
-
-//////////////////
+const AppError = require('./../../utilities/appError');
 
 exports.getAllCountries = catchAsync(async (req, res, next) => {
 	const countries = await countryDB.find();
-	console.log(countries);
 	res.status(200).json({
 		status: 'success',
 		data: {
@@ -21,31 +14,8 @@ exports.getAllCountries = catchAsync(async (req, res, next) => {
 	next();
 });
 
-exports.addCity = catchAsync(async (req, res, next) => {
-	if (req.body.countryName) {
-		const country = await countryDB.findOne({
-			country_name: req.body.countryName,
-		});
-		if (country != null) {
-			await country.country_cities.push(req.body.cityName);
-		}
-		res.status(200).json({
-			status: 'success',
-			message: 'city was added succesfully',
-		});
-	} else {
-		res.status(404).json({
-			status: 'fail',
-			message: 'please insert the country',
-		});
-	}
-	return next();
-});
-
 // age yebar country ro add konim , va dafe dige hamun country ro entexab konim ke shahri behesh ezafe konim , lazeme bazam country name vared konim ? age nakonim be megdar jadid (ke null hast) update mishe ya gabli mimune ?
 exports.postAddCountry = catchAsync(async (req, res, next) => {
-	console.log(req);
-	console.log(req.files);
 	const inputs = {
 		countryName: req.body.countryName,
 		countryCities: req.body.cityName,
@@ -62,3 +32,43 @@ exports.postAddCountry = catchAsync(async (req, res, next) => {
 		status: 'success',
 	});
 });
+
+exports.addCity = catchAsync(async (req, res, next) => {
+	if (req.body.countryName) {
+		const country = await countryDB.findOne({
+			country_name: req.body.countryName,
+		});
+		if (!country) {
+			return next(new AppError('country not found', 404));
+		} else {
+			await country.country_cities.push(req.body.cityName);
+		}
+		res.status(200).json({
+			status: 'success',
+			message: 'city was added succesfully',
+		});
+	} else {
+		res.status(404).json({
+			status: 'fail',
+			message: 'please insert the country',
+		});
+	}
+	return next();
+});
+
+/// get all cities of given country
+exports.getAllCities = catchAsync(async (req, res, next) => {
+	const country = await countryDB.findOne({
+		country_name: req.params.countryName,
+	});
+	if (!country) {
+		return next(new AppError('country not found', 404));
+	} else {
+		res.status(200).json({
+			status: 'sucess',
+			data: country.country_cities,
+		});
+	}
+});
+
+// exports.getEstatesOfCountry =
